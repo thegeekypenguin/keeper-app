b20861941ccaee3452647a6c08f5e63b319cb6d3
Shubham Sahay <pihusahay9999@gmail.com>
2023-01-29 01:33:31 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(){
+    // Creating a '<header>' component here:
+    return <header>
+         <h1>Keeper</h1>
+    </header>;
+    
+}
+
+export default Header;
